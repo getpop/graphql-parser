963dedd48f3a95541b8dfc9d6987951b0b7a7e88
leoloso <leo@getpop.org>
2021-01-03 11:35:23 +0000

    Updated description

---
+++ src/Exception/Parser/SyntaxErrorException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Date: 23.11.15
+ *
+ * @author Portey Vasil <portey@gmail.com>
+ */
+
+namespace GraphQLByPoP\GraphQLParser\Exception\Parser;
+
+class SyntaxErrorException extends AbstractParserError
+{
+
+}
