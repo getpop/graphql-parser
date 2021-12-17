2ea331da26c975254dc52886f2c9900147f2c4ff
leoloso <leo@getpop.org>
2021-12-17 02:22:25 +0000

    Merge pull request #1295 from leoloso/Extend-Field-and-Directive
    Extended Field from Backbone

--- src/Parser/Ast/Directive.php
+++ src/Parser/Ast/Directive.php
@@ -2,30 +2,10 @@
 
 declare(strict_types=1);
 
-namespace PoPBackbone\GraphQLParser\Parser\Ast;
+namespace PoP\GraphQLParser\Parser\Ast;
 
-use PoPBackbone\GraphQLParser\Parser\Location;
+use PoPBackbone\GraphQLParser\Parser\Ast\Directive as UpstreamDirective;
 
-class Directive extends AbstractAst
+class Directive extends UpstreamDirective
 {
-    use AstArgumentsTrait;
-
-    /**
-     * @param Argument[] $arguments
-     */
-    public function __construct(private $name, array $arguments, Location $location)
-    {
-        parent::__construct($location);
-        $this->setArguments($arguments);
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
 }
--- src/Parser/Ast/Field.php
+++ src/Parser/Ast/Field.php
@@ -2,44 +2,10 @@
 
 declare(strict_types=1);
 
-namespace PoPBackbone\GraphQLParser\Parser\Ast;
+namespace PoP\GraphQLParser\Parser\Ast;
 
-use PoPBackbone\GraphQLParser\Parser\Ast\Interfaces\FieldInterface;
-use PoPBackbone\GraphQLParser\Parser\Location;
+use PoPBackbone\GraphQLParser\Parser\Ast\Field as UpstreamField;
 
-class Field extends AbstractAst implements FieldInterface
+class Field extends UpstreamField
 {
-    use AstArgumentsTrait;
-    use AstDirectivesTrait;
-
-    /**
-     * @param Argument[] $arguments
-     * @param Directive[] $directives
-     */
-    public function __construct(private string $name, private ?string $alias, array $arguments, array $directives, Location $location)
-    {
-        parent::__construct($location);
-        $this->setArguments($arguments);
-        $this->setDirectives($directives);
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
-    public function getAlias(): ?string
-    {
-        return $this->alias;
-    }
-
-    public function setAlias(?string $alias): void
-    {
-        $this->alias = $alias;
-    }
 }
