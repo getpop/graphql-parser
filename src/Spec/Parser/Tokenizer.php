b9df27af81e66bc8f2ad5476f2144604aa2f414f
leoloso <leo@getpop.org>
2022-01-27 10:59:07 +0000

    Merge pull request #1415 from leoloso/Move-ErrorMessage-functions-to-new-service-GraphQLErrorMessageProvider
    Move `ErrorMessage` functions to new service `GraphQLErrorMessageProvider`

--- src/Spec/Parser/Tokenizer.php
+++ src/Spec/Parser/Tokenizer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\GraphQLParser\Spec\Parser;
 
+use PoP\GraphQLParser\Error\GraphQLErrorMessageProviderInterface;
 use PoP\GraphQLParser\Exception\Parser\SyntaxErrorException;
 use PoP\Root\Services\BasicServiceTrait;
 
@@ -15,8 +16,18 @@ class Tokenizer
     protected int $pos = 0;
     protected int $line = 1;
     protected int $lineStart = 0;
-
     protected Token $lookAhead;
+
+    private ?GraphQLErrorMessageProviderInterface $graphQLErrorMessageProvider = null;
+
+    final public function setGraphQLErrorMessageProvider(GraphQLErrorMessageProviderInterface $graphQLErrorMessageProvider): void
+    {
+        $this->graphQLErrorMessageProvider = $graphQLErrorMessageProvider;
+    }
+    final protected function getGraphQLErrorMessageProvider(): GraphQLErrorMessageProviderInterface
+    {
+        return $this->graphQLErrorMessageProvider ??= $this->instanceManager->getInstance(GraphQLErrorMessageProviderInterface::class);
+    }
 
     protected function initTokenizer(string $source): void
     {
@@ -154,7 +165,7 @@ class Tokenizer
             return $this->scanString();
         }
 
-        throw $this->createException('Can\t recognize token type');
+        throw $this->createException($this->getGraphQLErrorMessageProvider()->getCantRecognizeTokenTypeErrorMessage());
     }
 
     protected function checkFragment(): bool
@@ -326,13 +337,13 @@ class Tokenizer
                     case 'u':
                         $codepoint = substr($this->source, $this->pos + 1, 4);
                         if (!preg_match('/[0-9A-Fa-f]{4}/', $codepoint)) {
-                            throw $this->createException(sprintf($this->__('Invalid string unicode escape sequece \'%s\'', 'graphql-server'), $codepoint));
+                            throw $this->createException($this->getGraphQLErrorMessageProvider()->getInvalidStringUnicodeEscapeSequenceErrorMessage($codepoint));
                         }
                         $ch = html_entity_decode("&#x{$codepoint};", ENT_QUOTES, 'UTF-8');
                         $this->pos += 4;
                         break;
                     default:
-                        throw $this->createException(sprintf($this->__('Unexpected string escaped character \'%s\'', 'graphql-server'), $ch));
+                        throw $this->createException($this->getGraphQLErrorMessageProvider()->getUnexpectedStringEscapedCharacterErrorMessage($ch));
                 }
             }
 
@@ -374,6 +385,6 @@ class Tokenizer
      */
     protected function createUnexpectedTokenTypeException($tokenType)
     {
-        return $this->createException(sprintf($this->__('Unexpected token \'%s\'', 'graphql-server'), Token::tokenName($tokenType)));
+        return $this->createException($this->getGraphQLErrorMessageProvider()->getUnexpectedTokenErrorMessage(Token::tokenName($tokenType)));
     }
 }
